923cb2e3fd6882535c922a9c96a7f4d6d3198671
Murat Yaşar <murat.yasar@comkom.de>
2024-05-20 20:25:57 +0200

    Build client-side of email-app

---
+++ email-app/fe/src/App.js
@@ -0,0 +1,12 @@
+
+import EmailForm from './components/EmailForm/EmailForm';
+
+function App() {
+  return (
+    <div className="App">
+      <EmailForm></EmailForm>
+    </div>
+  );
+}
+
+export default App;
