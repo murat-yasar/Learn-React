b900ebfc91977e99aad902f89002d1c062f2bb19
Murat Yaşar <murat.yasar@comkom.de>
2024-05-21 12:38:44 +0200

    Build list component

--- contact-app/src/components/Contacts/List/index.js
+++ contact-app/src/components/Contacts/List/index.js
@@ -1,9 +1,14 @@
 import React from 'react'
 
-function List() {
+function List({ contacts }) {
   return (
     <div>
       <h1>Contact List</h1>
+      <ul>
+        {
+          contacts.map((contact, i) => <li key={i}>{contact.fullName} {contact.phoneNumber}</li>)
+        }
+      </ul>
     </div>
   )
 }
