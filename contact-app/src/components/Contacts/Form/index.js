29e1a3d33ab0d037fda001fa70bcb13d13493d66
Murat Yaşar <murat.yasar@comkom.de>
2024-05-21 12:27:05 +0200

    Build form component

--- contact-app/src/components/Contacts/Form/index.js
+++ contact-app/src/components/Contacts/Form/index.js
@@ -1,9 +1,56 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
-function Form() {
+// Set initial form values
+const initFormVals = {fullName: "", phoneNumber: ""};
+
+function Form({ addContact, contacts }) {
+  const [form, setForm] = useState(initFormVals);
+
+  // Reset form-fields after changing contacts
+  useEffect(() => {
+    setForm(initFormVals);
+  }, [contacts]);
+
+  // onChangeInput
+  const onChangeForm = (e) => {
+    setForm({ ...form, [e.target.name]: e.target.value });
+  }
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+
+    // Do not proceed, unless the both field fulfilled
+    if (form.fullName === '' || form.phoneNumber === '') return false;
+
+    // Add new contact to the contacts
+    addContact([...contacts, form]);
+
+    // Reset form-fields after submit
+    // setForm(initFormVals);
+  }
+
   return (
     <div>
-      <h1>Add Contact Form</h1>
+      <form onSubmit={onSubmit}>
+        <input 
+          type="text" 
+          name='fullName' 
+          value={form.fullName}
+          placeholder='Full Name' 
+          onChange={onChangeForm}
+        />
+        <br />
+        <input 
+          type="tel" 
+          name='phoneNumber' 
+          value={form.phoneNumber}
+          placeholder='Phone Number' 
+          onChange={onChangeForm}
+        />
+        <br />
+        <br />
+        <button>Add Contact</button>
+      </form>
     </div>
   )
 }
--- contact-app/src/components/Contacts/index.js
+++ contact-app/src/components/Contacts/index.js
@@ -1,12 +1,19 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import List from './List'
 import Form from './Form';
 
 function Contacts() {
+  const [contacts, setContacts] = useState([]);
+
+  // Get the change if contacts changes
+  useEffect(()=>{
+    console.log(contacts);
+  }, [contacts]);
+  
   return (
     <div>
       <List />
-      <Form />
+      <Form addContact={setContacts} contacts={contacts} />
     </div>
   )
 }
