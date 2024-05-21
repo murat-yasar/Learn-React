742d1952b249bce900488d836a3bcee0f57dd693
Murat Yaşar <murat.yasar@comkom.de>
2024-05-21 18:23:08 +0200

    init todo-app

---
+++ todo-app/src/components/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Footer() {
+  return (
+    <footer class="info">
+      <p>Click to edit a todo</p>
+      <p>Created by <a href="https://github.com/murat-yasar">Murat Yaşar</a> </p>
+    </footer>
+  )
+}
+
+export default Footer;
---
+++ todo-app/src/components/Header.js
@@ -0,0 +1,12 @@
+import './../index.css';
+import React from 'react';
+
+function Header() {
+  return (
+    <header className="header">
+		  <h1>todos</h1>
+	  </header>
+  )
+}
+
+export default Header;
