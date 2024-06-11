656169ea0c73ac202e65d138bcddbb39d05084eb
Murat Yaşar <murat.yasar@comkom.de>
2024-06-11 12:24:43 +0200

    Add dropdown, checkbox, radio. Style table.

--- react-formik/src/components/Form/index.js
+++ react-formik/src/components/Form/index.js
@@ -12,6 +12,9 @@ const Form = () => {
           firstName: '',
           lastName: '',
           email: '',
+          language: '',
+          gender: '',
+          skills: []
         }}
         onSubmit={async (values) => {
           await new Promise((r) => setTimeout(r, 500));
@@ -20,18 +23,81 @@ const Form = () => {
       >
         {( { handleChange, handleSubmit } )=>(
           <form onSubmit={handleSubmit}>
-            <label htmlFor='firstName'>First Name</label>
-            <input name='firstName' type='text' onChange={handleChange} />
+
+          <table>
+              <th>Personal Details</th>
+              <tr>
+                <td> <label htmlFor='firstName'>First Name: </label></td>
+                <td><input id='firstName' name='firstName' type='text' className={styles.textField} onChange={handleChange} /></td>
+              </tr>
+              <tr>
+                <td> <label htmlFor='lastName'>Last Name: </label></td>
+                <td><input id='lastName' name='lastName' type='text' className={styles.textField} onChange={handleChange} /></td>
+              </tr>
+              <tr>
+                <td><label htmlFor='email'>Email: </label></td>
+                <td><input id='email' name='email' type="email" className={styles.textField} onChange={handleChange} /></td>
+              </tr>
+              <tr>
+                <td>Language</td>
+                <td>
+                  <select name='language' >
+                    <option value='TR' onChange={handleChange}>Türkçe</option>
+                    <option value='DE' onChange={handleChange}>Deutsch</option>
+                    <option value='EN' onChange={handleChange}>English</option>
+                  </select>
+                </td>
+              </tr>
+              <tr>
+                <td><label htmlFor='gender'>Gender: </label></td>
+                <td>
+                  <span>male</span>
+                  <input name='gender' type='radio' value='male' onChange={handleChange} />
+                  <span>female</span>
+                  <input name='gender' type='radio' value='female' onChange={handleChange} />
+                </td>
+              </tr>
+
+              <tr></tr>
+            {/* </table>
 
             <br/><br/>
 
-            <label htmlFor='lastName'>Last Name</label>
-            <input name='lastName' type='text' onChange={handleChange} />
-
-            <br/><br/>
-
-            <label htmlFor='email'>Email</label>
-            <input name='email' type="email" onChange={handleChange} />
+            <table> */}
+              <th>IT Skills</th>
+              <tr>
+                <td><input name='skills' type='checkbox' value='HTML' onChange={handleChange} /></td>
+                <td>HTML</td>
+              </tr>
+              <tr>
+                <td><input name='skills' type='checkbox' value='CSS' onChange={handleChange} /></td>
+                <td>CSS</td>
+              </tr>
+              <tr>
+                <td><input name='skills' type='checkbox' value='JavaScript' onChange={handleChange} /></td>
+                <td>JavaScript</td>
+              </tr>
+              <tr>
+                <td><input name='skills' type='checkbox' value='React' onChange={handleChange} /></td>
+                <td>React</td>
+              </tr>
+              <tr>
+                <td><input name='skills' type='checkbox' value='NodeJS' onChange={handleChange} /> </td>
+                <td>NodeJS</td>
+              </tr>
+              <tr>
+                <td><input name='skills' type='checkbox' value='MongoDB' onChange={handleChange} /></td>
+                <td>MongoDB</td>
+              </tr>
+              <tr>
+                <td><input name='skills' type='checkbox' value='MySQL' onChange={handleChange} /></td>
+                <td>MySQL</td>
+              </tr>
+              <tr>
+                <td><input name='skills' type='checkbox' value='MariaDB' onChange={handleChange} /></td>
+                <td>MariaDB</td>
+              </tr>
+            </table>
 
             <br/><br/>
             <button type='submit'>Submit</button>
